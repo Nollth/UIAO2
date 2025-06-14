049b7235cbeacbfb4928f401b754892a2ea554ae
NothA <nothandnull@gmail.com>
2025-06-14 13:39:58 -0300

    Important!

--- src/main/java/com/nothandnull/upgradableitemsandorestwo/UpgradableItemsAndOresTwo.java
+++ src/main/java/com/nothandnull/upgradableitemsandorestwo/UpgradableItemsAndOresTwo.java
@@ -3,10 +3,7 @@ package com.nothandnull.upgradableitemsandorestwo;
 import com.mojang.logging.LogUtils;
 import com.nothandnull.upgradableitemsandorestwo.block.ModBlocks;
 import com.nothandnull.upgradableitemsandorestwo.item.ModCreativeModTabs;
-import com.nothandnull.upgradableitemsandorestwo.item.ModFoods;
 import com.nothandnull.upgradableitemsandorestwo.item.ModItems;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -20,45 +17,37 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(UpgradableItemsAndOresTwo.MOD_ID)
-public class UpgradableItemsAndOresTwo
-{
+public class UpgradableItemsAndOresTwo {
     public static final String MOD_ID = "upgradableitemsandorestwo";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public UpgradableItemsAndOresTwo(FMLJavaModLoadingContext context)
-    {
-        IEventBus modEventBus = context.getModEventBus();
+    public UpgradableItemsAndOresTwo() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
 
         ModCreativeModTabs.register(modEventBus);
-
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
     }
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
     }
 
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
         }
     }
 }
